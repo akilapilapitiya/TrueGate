445296741fcd7f352cdabf84f007015532f1b1fb
Akila <akilapilapitiya4@gmail.com>
2025-06-30 17:03:17 +0530

    feat: FailModel Changed to mui

--- frontend/src/components/modals/FailModal.jsx
+++ frontend/src/components/modals/FailModal.jsx
@@ -1,14 +1,31 @@
-const SuccessModal = ({ message, onClose }) => {
+import React from "react";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from "@mui/material";
+import { colorPallete } from "../../ColorTheme";
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
+const FailModal = ({ message, onClose }) => {
   return (
-    <div className="modal-overlay">
-      <div className="fail-modal">
-        <h2>Failed</h2>
-        <p>{message}</p>
-        <button onClick={onClose} className="modal-close-btn">
+    <Dialog open onClose={onClose} aria-labelledby="success-dialog-title" >
+      <DialogTitle id="success-dialog-title" sx={{background:colorPallete.successModalBackgroundColor, color: colorPallete.successPageNormalText, textAlign:'center', fontWeight:'bold', fontSize:'2rem'}}>ACTION FAILED <ErrorOutlineIcon sx={{ color: colorPallete.successPageNormalText, textAlign:'center', fontWeight:'bold', fontSize:'2rem'}}/></DialogTitle>
+      <DialogContent dividers sx={{background:colorPallete.successModalBackgroundColor, border:'none'}}>
+        <Typography sx={{color: colorPallete.successPageNormalText}}>{message}</Typography>
+      </DialogContent>
+      <DialogActions sx={{background:colorPallete.successModalBackgroundColor }}>
+        <Button onClick={onClose} variant="contained" 
+        sx={{
+                            background: colorPallete.registerButtonColor,
+                            color: colorPallete.registerButtonAccentColor,
+                            borderColor: colorPallete.registerButtonAccentColor,
+                            "&:hover": {
+                              background: colorPallete.registerButtonHoverColor,
+                              color: colorPallete.registerButtonHoverAccentColor,
+                              borderColor: colorPallete.registerButtonHoverAccentColor,
+                            },
+                          }}>
           Close
-        </button>
-      </div>
-    </div>
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
