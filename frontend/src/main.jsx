211edf046f460ebb698e35a6d54d6a4e663c0048
Tharuka Subhashi <tharukasubhashi2002@gmail.com>
2025-07-04 17:40:21 +0530

    Fix Errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,12 @@ import App from "./App";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
 import AppStore from "./utils/AppStore";
-import { theme } from "./utils/ColorTheme";
+//import { getTheme, lightColors  } from "./ColorTheme.jsx";
 
 
 createRoot(document.getElementById("root")).render(
   <Provider store={AppStore}>
-    <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-    </ThemeProvider>
   </Provider>
 );
