fdc6ab8614762a198bf70952d234c11eb1d980e7
Akila Pilapitiya <156910086+akilapilapitiya@users.noreply.github.com>
2025-07-05 17:01:39 +0530

    Added template and Assets for profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,13 +1,45 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Grid, Paper, Typography } from "@mui/material";
+import maleIcon from "../assets/maleIcon.png";
 
 const Profile = () => {
   return (
     <Box
-      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      sx={{
+        width: "100vw",
+        minHeight: "100vh",
+        backgroundColor: "rgb(255, 255, 255)",
+        padding: { xs: 2, sm: 3, md: 4 },
+        boxSizing: "border-box",
+        margin: 0,
+      }}
     >
-      <Typography variant="h4" sx={{ textAlign: "center" }}>
-        Profile
-      </Typography>
+      <Box
+        sx={{
+          background: "blue",
+          alignItems: "center",
+          display: "flex",
+          ustifyContent: "center",
+          paddingTop: "10vh",
+        }}
+      >
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={4} md={4} lg={3}>
+            <Paper
+              elevation={2}
+              sx={{
+                minWidth: "300px",
+              }}
+            >
+              <Container>
+                <img src={maleIcon} alt="" />
+              </Container>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={8} md={8} lg={3}>
+            <Paper elevation={2}>Helllo there</Paper>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 };
